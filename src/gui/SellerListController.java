a167fa255ec71799e97db0f71f97fad42f581bd5
Romldo A. Lima Jr <odlimor.sevla@gmail.com>
2023-06-07 12:05:49 -0300

    TextField & DatePicker

--- src/gui/SellerListController.java
+++ src/gui/SellerListController.java
@@ -70,7 +70,7 @@ public class SellerListController implements Initializable, DataChangeListener {
 	public void onBtNewAction(ActionEvent event) {
 		Stage parentStage = Utils.currentStage(event);
 		Seller obj = new Seller();
-		//createDialogForm(obj, "/gui/SellerForm.fxml", parentStage);
+		createDialogForm(obj, "/gui/SellerForm.fxml", parentStage);
 	}
 
 	public void setSellerService(SellerService service) {
